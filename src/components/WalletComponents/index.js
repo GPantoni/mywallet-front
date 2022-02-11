d1fd22eb109803455ac75d688186100372bb3fc5
mateuskuritza <mateuskuritza@gmail.com>
2022-02-11 10:31:19 -0300

    Preparando revisão de código

--- src/components/WalletComponents/index.js
+++
@@ -1,7 +0,0 @@
-import Container from "./Container";
-import Content from "./Content";
-import Header from "./Header";
-import Footer from "./Footer";
-import EmptyContent from "./EmptyContent";
-
-export { Container, Content, Header, Footer, EmptyContent };
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import Login from "./Login";
-import Register from "./Register";
-import Wallet from "./Wallet";
-
-export { Login, Register, Wallet };
